a081f039c35a0cd499da8697e3a85cecd225a2b3
sukalyansen123 <sukalyansen123@gmail.com>
2020-04-23 00:57:29 +0530

    Make transport upgrade false for testing

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+  transports: ['websocket'],
+  allowUpgrades: false,
+  pingInterval: 25000,
+  pingTimeout: 60000
+});
+
 var port = process.env.PORT || 5000;
 
 const levenshtein = require('js-levenshtein');
